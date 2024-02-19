dfcb201bbd47c97db56fbf00189435b50896e8e8
valehash <newlechim@gmail.com>
2024-02-19 15:02:32 +0000

    bubble sort is bubbling

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,34 +8,36 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
+	size_t k;
 	size_t j;
 	int temp;
 	int swapped;
 
-	i = 0;
 
-	for (j = 0; j < size -1; j++)
+	for (j = 0; j < size - 1; j++)
 	{
 		swapped = 0;
-
-		for(i = 0; i < size -1; i++)
+		for(i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				temp = array[i + 1];
 				array[i + 1] = array[i];
 				array[i] = temp;
+
 				swapped = 1;
+
+				for(k = 0; k < size; k++)
+				{
+					if (k == size -1)
+						printf("%d\n", array[k]);
+					else
+						printf("%d ,",array[k]);
+				}
 			}
 		}
-		if (swapped)
-		{
-			printf("%d", array[0]);
-			for (i = 1; i < size; i++)
-			{
-				printf(", %d", array[i]);
-			}
-			printf("\n");
-		}
+		if (swapped == 0)
+			break;
 	}
+
 }
