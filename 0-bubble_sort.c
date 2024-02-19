190f3548490c5a7db931bd6d987b3f7cff09c3f5
valehash <newlechim@gmail.com>
2024-02-19 15:44:56 +0000

    Null bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,8 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	int swapped;
 
+	if (size < 1)
+		return;
 
 	for (j = 0; j < size - 1; j++)
 	{
--- 0-main.c
+++ 0-main.c
@@ -9,13 +9,14 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    /*int array[] = {};
     size_t n = sizeof(array) / sizeof(array[0]);
+	*/
 
-    print_array(array, n);
+    /*print_array(array, n);*/
     printf("\n");
-    bubble_sort(array, n);
+    bubble_sort(NULL, 0);
     printf("\n");
-    print_array(array, n);
+    /*print_array(array, n);*/
     return (0);
 }
