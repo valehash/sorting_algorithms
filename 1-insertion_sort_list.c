d5b7e8923ed1335da46252200525dd1868352c2a
valehash <newlechim@gmail.com>
2024-02-20 17:33:10 +0000

    betty insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,9 @@
 #include "sort.h"
+/**
+ * insertion_sort_list - function that sorts a doubly linked list
+ * @list: The hwad of the linked list
+ */
 
- 
 
 void insertion_sort_list(listint_t **list)
 {
@@ -8,36 +11,31 @@ void insertion_sort_list(listint_t **list)
 	listint_t *prev;
 
 	current = (*list)->next;
-	
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	while (current != NULL)
 	{
 		prev = current->prev;
-	
-	while (prev != NULL && prev->n > current->n)
-        {
 
-            prev->next = current->next;
-            if (current->next != NULL)
-                current->next->prev = prev;
-            current->next = prev;
-            current->prev = prev->prev;
-            if (prev->prev != NULL)
-                prev->prev->next = current;
-            prev->prev = current;
+		while (prev != NULL && prev->n > current->n)
+		{
 
+			prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = prev;
+			current->next = prev;
+			current->prev = prev->prev;
 
-            prev = current->prev;
-            if (prev == NULL)
-                *list = current;
-            
+			if (prev->prev != NULL)
+				prev->prev->next = current;
 
-            print_list(*list);
-        }
-
-
-        current = current->next;
-    }
+			prev->prev = current;
+			prev = current->prev;
+			if (prev == NULL)
+				*list = current;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
-
